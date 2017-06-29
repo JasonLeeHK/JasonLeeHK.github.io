546ae146d01845ff7e4a6c8da4bbc8b5643021ca
Jason <a190654@Jasons-MacBook-Pro-4.local>
2017-06-29 17:36:09 +0900

    major changes

--- javascript/main.js
+++ javascript/main.js
@@ -1,17 +1,7 @@
 $( document ).ready(function() {
     console.log( "ready!" );
-    var flag= true;
-    $('#mybutton').click(function(){	
-    	if (flag==true) {
-    		$('#name').css('color', 'red');
-    		flag=false;
-		}
-   		else {
-    		$('#name').css('color', '#007eab');
-    		flag=true;
-		}
-	
+   
     });
-    });
+    };
 
 
