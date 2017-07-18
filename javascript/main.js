9a0dfbff9718e821ce28f8dca61898d503c24d9e
Jason <a190654@Jasons-MacBook-Pro-4.local>
2017-07-18 18:15:40 +0900

    huge change

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,24 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
-   
-    });
-    };
+
+$(document).ready(function(){
+  $('.autoplay').slick({
+  	slidesToShow: 3,
+  	slidesToScroll: 1,
+  	autoplay: true,
+  	autoplaySpeed: 800, 
+	});
+
+  
+$('.fade').slick({
+  dots: false,
+  infinite: true,
+  speed: 500,
+  autoplay: true,
+  autoplaySpeed: 1000,
+  fade: true,
+  cssEase: 'linear'
+});
 
 
+
+		
+});
